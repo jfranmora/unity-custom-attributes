c2d38baf8af2a0beecb12600ff61b3a151322574
Juan Mora <Juan.Mora@mediatonic.co.uk>
2020-02-07 09:17:43 +0000

    ReadOnlyAttribute

---
+++ Assets/ReadOnlyAttribute/Editor/ReadOnlyProperyDrawer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyProperyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        bool enabledState = GUI.enabled;
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label);
+        GUI.enabled = enabledState;
+
+        EditorGUI.EndProperty();
+    }
+}
---
+++ Assets/ReadOnlyAttribute/Examples/ExampleReadOnlyAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleReadOnlyAttribute : MonoBehaviour
+{
+    public int a;
+    [ReadOnly] public float b;
+    public string c;
+}
